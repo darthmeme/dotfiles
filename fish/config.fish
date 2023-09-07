ca5c38cccf597f478324f772b91a15461965be49
Akira Laine <akira_laine@outlook.com>
2023-09-07 19:18:22 +0900

    feat(fish): ^c to remove whole line

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,8 @@ set -U fish_greeting
 set -U fish_key_bindings fish_vi_key_bindings
 set -Ux EDITOR nvim
 
+bind -M insert \cc kill-whole-line
+
 fish_add_path ~/.config/bin
 
 zoxide init fish | source
