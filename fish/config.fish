6ce6d1d1bb914db7c2a74b3ab4081bf6dd1c2809
Akira Laine <akira_laine@outlook.com>
2023-09-07 19:37:33 +0900

    feat(fish): added ^c for normal mode

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ set -U fish_greeting
 set -U fish_key_bindings fish_vi_key_bindings
 set -Ux EDITOR nvim
 
+bind -m insert \cc kill-whole-line repaint
 bind -M insert \cc kill-whole-line
 
 fish_add_path ~/.config/bin
