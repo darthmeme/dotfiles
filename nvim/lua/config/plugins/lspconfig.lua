59e0f2110503b74f683773fa4008cd720b279d5f
Akira Laine <akira_laine@outlook.com>
2023-09-13 16:40:24 +0900

    fix(nvim): use volar takeover mode, find typescript in parent folders

--- nvim/lua/config/plugins/lspconfig.lua
+++ nvim/lua/config/plugins/lspconfig.lua
@@ -6,6 +6,7 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
+    local util = require("lspconfig.util")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local keymap = vim.keymap
     local opts = { noremap = true, silent = true }
@@ -20,10 +21,22 @@ return {
 
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    lspconfig["tsserver"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach
-    })
+    local function get_typescript_server_path(root_dir)
+      local global_ts = '/home/akiralaine/.npm/lib/node_modules/typescript/lib'
+      local found_ts = ''
+      local function check_dir(path)
+        found_ts =  util.path.join(path, 'node_modules', 'typescript', 'lib')
+        if util.path.exists(found_ts) then
+          return path
+        end
+      end
+      if util.search_ancestors(root_dir, check_dir) then
+        return found_ts
+      else
+        return global_ts
+      end
+    end
+
 
     lspconfig["html"].setup({
       capabilities = capabilities,
@@ -57,9 +70,14 @@ return {
       on_attach = on_attach
     })
 
+
     lspconfig["volar"].setup({
       capabilities = capabilities,
-      on_attach = on_attach
+      on_attach = on_attach,
+      filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+      on_new_config = function(new_config, new_root_dir)
+        new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+      end,
     })
 
     lspconfig["eslint"].setup({
@@ -84,6 +102,11 @@ return {
     })
 
     vim.cmd [[
+      highlight! DiagnosticLineNrError guibg=#51202A guifg=#FF0000 gui=bold
+      highlight! DiagnosticLineNrWarn guibg=#51412A guifg=#FFA500 gui=bold
+      highlight! DiagnosticLineNrInfo guibg=#1E535D guifg=#00FFFF gui=bold
+      highlight! DiagnosticLineNrHint guibg=#1E205D guifg=#A3A3FF gui=bold
+
       sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
       sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
       sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=DiagnosticLineNrInfo
