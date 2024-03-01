7c125a9104ebe7fb0d1c6ced04efc4f633a59040
Akira Laine <akira_laine@outlook.com>
2024-03-01 23:57:33 +1100

    feat(fish): added nvm auto switch

--- fish/config.fish
+++ fish/config.fish
@@ -15,3 +15,20 @@ fish_add_path /Users/akiralaine/Library/Python/3.11/bin/
 
 zoxide init fish | source
 starship init fish | source
+
+function __check_nvm --on-variable PWD --description 'Do nvm stuff'
+  if test -f .nvmrc
+    set node_version (node -v)
+    set nvmrc_node_version (nvm list | grep (cat .nvmrc))
+
+    if set -q $nvmrc_node_version
+      nvm install
+    else if string match -q -- "*$node_version" $nvmrc_node_version
+      # already current node version
+    else
+      nvm use -s
+    end
+  end
+end
+
+__check_nvm
