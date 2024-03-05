a8f25870336f7b572a894a1b772a3c133751c950
Akira Laine <akira_laine@outlook.com>
2024-03-05 14:01:42 +1100

    fix(fish) always prepend default nvm node version to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -16,7 +16,20 @@ fish_add_path /Users/akiralaine/Library/Python/3.11/bin/
 zoxide init fish | source
 starship init fish | source
 
+function _nvm_version_match --argument-names ver
+    string replace --regex -- '^v?(\d+|\d+\.\d+)$' 'v$1.' $ver |
+        string replace --filter --regex -- '^v?(\d+)' 'v$1' |
+        string escape --style=regex || string lower '\b'$ver'(?:/\w+)?$'
+end
+
 function __check_nvm --on-variable PWD --description 'Do nvm stuff'
+  # Re-prepend the default node version to PATH.
+  # Fixes issue with another binary that has its own Node version
+  # being used instead of nvm's Node.
+  set --local default_node_path (ls $nvm_data | string match --entire --regex -- (_nvm_version_match $nvm_default_version))
+
+  set --prepend PATH $nvm_data/$default_node_path/bin
+
   if test -f .nvmrc
     set node_version (node -v)
     set nvmrc_node_version (nvm list | grep (cat .nvmrc))
