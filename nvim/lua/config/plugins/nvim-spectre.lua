0c22d23937ab051520a4cf922e323e4aa8097756
Akira Laine <akira_laine@outlook.com>
2024-10-25 22:28:43 +1100

    feat(nvim): updated spectre config

--- nvim/lua/config/plugins/nvim-spectre.lua
+++ nvim/lua/config/plugins/nvim-spectre.lua
@@ -1,3 +1,21 @@
 return {
-  'nvim-pack/nvim-spectre'
+  'nvim-pack/nvim-spectre',
+  config = function()
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>r", "<cmd>Spectre<cr>")
+
+    require('spectre').setup({
+      replace_engine = {
+        ["sed"] = {
+          cmd = "sed",
+          args = {
+            "-i",
+            "",
+            "-E",
+          },
+        },
+      },
+    })
+  end
 }
